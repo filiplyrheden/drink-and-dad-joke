30af80efc923424a42c47cda5d1dc79e56ea216c
filiplyrheden <filip.lyrheden@gmail.com>
2024-12-10 15:04:46 +0100

    Implemented function to sober-button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
-const button = document.querySelector('#dad-button')
+const dadButton = document.querySelector('#dad-button')
+const soberButton = document.querySelector('#sober-button')
 const drinkTitle = document.querySelector('#drink-title')
 const ingredients = document.querySelector('#ingredients')
 const drinkInstructions = document.querySelector('#instructions')
@@ -8,8 +9,68 @@ const contentContainer = document.querySelector('.content-container');
 const converterContainer = document.querySelector('.converter-container');
 
 const randomCocktail = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+const nonAlcoholicCockatail = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic'
 
 //Cocktail API
+
+//Displays non-alcoholic cocktail
+function displayNonAlcoholicDrink () {
+  fetch(nonAlcoholicCockatail)
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      const drink = data.drinks;
+
+//Randomizes the index
+const randomIndex = Math.floor(Math.random() * drink.length);
+const randomDrink = drink[randomIndex];
+const randomDrinkId = randomDrink['idDrink']
+
+//Endpoint where cocktails are listed by ID, with random non-alcoholic cocktail id inserter in the url.
+const cocktailById = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${randomDrinkId}`;
+
+fetch(cocktailById)
+.then(response => {
+  return response.json()
+})
+.then(data => {
+  const drink = data.drinks[0]
+
+console.log(drink);
+
+drinkTitle.textContent = drink.strDrink
+      drinkInstructions.textContent = drink.strInstructions
+
+      ingredients.innerHTML = ''
+
+      for (let i = 1; i <= 15; i++) {
+        const ingredient = drink[`strIngredient${i}`]
+        const measure = drink[`strMeasure${i}`]
+
+        if (ingredient) {
+          const listItem = document.createElement('li')
+          listItem.textContent = measure
+            ? `${measure} ${ingredient}`
+            : ingredient
+          ingredients.appendChild(listItem)
+        }
+      }
+
+      //Resizes img to a fixed size
+      function resizeImage() {
+        drinkImage.style.width = "250px"; 
+        drinkImage.style.height = "250px"; 
+        drinkImage.style.objectFit = "cover"; 
+      }
+
+      drinkImage.src = drink.strDrinkThumb;
+      resizeImage();
+
+    })
+    })
+  }
+
 function displayRandomDrink () {
   fetch(randomCocktail)
     .then(response => {
@@ -53,7 +114,6 @@ function displayRandomDrink () {
 
 //Dad joke API
 const dadJokeApi = 'https://icanhazdadjoke.com/';
-const dadJokeButton = document.querySelector('#random-dad-joke-button');
 const dadJokeDisplay = document.querySelector('#dad-joke');
 
 function fetchDadJoke() {
@@ -68,9 +128,10 @@ function fetchDadJoke() {
     })
 }
 
-//Button
-button.addEventListener('click', displayRandomDrink);
-button.addEventListener('click', fetchDadJoke);
+//Buttons
+dadButton.addEventListener('click', displayRandomDrink);
+dadButton.addEventListener('click', fetchDadJoke);
+soberButton.addEventListener('click', displayNonAlcoholicDrink);
 
 //Changes CSS from display:none; to display:flex;
 document.getElementById('dad-button').addEventListener('click', function() {
@@ -81,7 +142,7 @@ document.getElementById('dad-button').addEventListener('click', function() {
 
 //Change button text on click
 document.getElementById('dad-button').addEventListener('click', function() {
-  button.textContent = "Dad still not taking you seriously? Try again!";
+  dadButton.textContent = "Dad still not taking you seriously? Try again!";
 });
 
   // Scroll to the content on click
